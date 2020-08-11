bf22bb9cd23c04fde96a1a59684ebefa76433717
DenyskoD <deniskodima1@gmail.com>
2020-08-11 16:13:50 +0300

    first start

---
+++ docs/assets/js/app.bbl.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log(1);
+var array = [1, 2, 3, 4, 5];
+array.forEach(function (elem) {
+  console.log(elem);
+});
---
+++ docs/assets/js/app.js
@@ -0,0 +1,7 @@
+console.log(1);
+
+let array = [1,2,3,4,5];
+
+array.forEach(elem => {
+    console.log(elem);
+});
