a9ae3d6e3f9e068661efb1391f6f67e9bbf22c61
DenyskoD <deniskodima1@gmail.com>
2020-08-11 16:34:49 +0300

    first HW

--- docs/assets/js/app.bbl.js
+++ docs/assets/js/app.bbl.js
@@ -1,7 +1,40 @@
-"use strict";
+/* Задание на урок:
 
-console.log(1);
-var array = [1, 2, 3, 4, 5];
-array.forEach(function (elem) {
-  console.log(elem);
-});
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+'use strict';
+
+var numberOfFilms;
+numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+var personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+var a = prompt("Один из последних просмотренных фильмов?", "");
+var b = prompt("На сколько оцените его?", "");
+var c = prompt("Один из последних просмотренных фильмов?", "");
+var d = prompt("На сколько оцените его?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
